0fdb8f3dd098b671b3bf5cfb0f3298a88051d525
AZIZXlaouiti <azizrocke@gmail.com>
2024-02-17 13:27:02 -0500

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
+import { useRoutes } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  // const [count, setCount] = useState(0)
+  const routes = useRoutes([
+    /*
+     * main page Home /
+     * view page 
+     * new page
+     * edit page
+    */
+    { path: "/", element: <div>home</div> },
+    { path: "/view/:id", element: <>page</>},
+    { path: "/new", element: <div>new</div> },
+    { path: "/edit/:id", element: <div>edit</div>},
+    { path: "*", element: <div>404 NOT FOUND</div> },
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {routes}
     </>
   )
 }
